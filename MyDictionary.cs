e317a3c6c303651ab43922c948d56d3c8116682d
Bekir Sancak <37635378+BekirSancak09@users.noreply.github.com>
2021-03-24 11:11:01 +0300

    Add files via upload

---
+++ MyDictionary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+  public class MyDictionary<TKey,TValue>
+    {
+        TKey[]tkeys;
+        TValue[] tvalues;
+       
+        public MyDictionary()
+        {
+            tkeys = new TKey[0];
+            tvalues = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] TkeyArray = tkeys;
+            TValue[] TvalueArray = tvalues;
+            tkeys = new TKey[tkeys.Length+1];
+            tvalues = new TValue[tvalues.Length + 1];
+            for (int i = 0; i < TkeyArray.Length; i++)
+            {
+                tkeys[i] = TkeyArray[i];
+            }
+
+            for (int i = 0; i < TvalueArray.Length; i++)
+            {
+                tvalues[i] = TvalueArray[i];
+            }
+            tkeys[tkeys.Length-1]= key;
+            tvalues[tvalues.Length - 1] = value;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> Ogrenci = new MyDictionary<int, string>();
+            Ogrenci.Add(134, "Tolga Demirer");
+            Ogrenci.Add(158, "Ümit Özkan");
+            Ogrenci.Add(115, "Kadir Aydemir");
+            Ogrenci.Add(174, "Cemal Çiftçi");
+
+            
+        }
+    }
+}
